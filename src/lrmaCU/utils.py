1a280c30191d2284a6c9553dc8cb3681852bdf85
Steve Huang <shuang@broadinstitute.org>
2023-10-03 11:14:01 -0400

    new function to allow FISS API re-tries

--- src/lrmaCU/utils.py
+++ src/lrmaCU/utils.py
@@ -2,6 +2,7 @@ import logging
 import os
 from typing import List
 
+import requests
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail, From, To, Subject, PlainTextContent, HtmlContent
 
@@ -42,6 +43,42 @@ def absolute_file_paths(directory):
     for dir_path, _, filenames in os.walk(directory):
         for f in filenames:
             yield os.path.abspath(os.path.join(dir_path, f))
+
+
+def retry_fiss_api_call(func_name: str, max_attempts: int, *args, **kwargs) -> requests.Response:
+    """
+    Re-try calling FISS API function for ConnectionResetError.
+    :param func_name: name of FISS API function
+    :param max_attempts: max retries + 1
+    :param kwargs: args to forward to the FISS API call
+    :return: request response object
+    """
+    from socket import error as SocketError
+    import errno
+    from firecloud import api as fapi
+    cnt = 0
+    response = requests.Response()
+    connection_reset = True
+    while max_attempts != cnt and connection_reset:
+        try:
+            connection_reset = False  # avoid unnecessary re-tries
+            # call FISS API function by its name
+            fiss_call = getattr(fapi, func_name)
+            response = fiss_call(*args, **kwargs)
+        except SocketError as e:  # but only retries for connect reset errors
+            if e.errno != errno.ECONNRESET:
+                logger.warning(f"Seeing error other than ConnectionRest during {cnt}-th attempt.")
+                break
+            else:
+                connection_reset = True
+                logger.warning(f"Seeing connection reset error for the {cnt}-th time.")
+        except Exception as ee:  # exit for all other types of errors
+            logger.error(f"Seeing error other than ConnectionRest during {cnt}-th attempt.")
+            break
+
+        cnt += 1
+
+    return response
 
 
 def send_notification(notification_sender_name: str,
